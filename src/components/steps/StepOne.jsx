3e87e202caa83d6b25526c1418e61b077d49ebff
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-01 20:56:45 -0300

    feat: created StepOne

--- src/components/steps/StepOne.jsx
+++ src/components/steps/StepOne.jsx
@@ -1,7 +1,47 @@
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { FormContext } from "../../formContext";
+import Navigation from "../Navigation";
+
 const StepOne = () => {
+  const { setCurrentStep } = useContext(FormContext);
+
+  const onSubmit = () => {
+    setCurrentStep((prevStep) => prevStep + 1);
+  };
+
   return (
-    <div className="rounded-md bg-white">
-      <h1>Step 1</h1>
+    <div>
+      {/* Form */}
+      <div className="-translate-y-[72px] px-4">
+        <div className="rounded-md bg-white px-6 py-7 shadow-sm">
+          <div>
+            <h1 className="text-marine-blue text-2xl font-bold">
+              Personal info
+            </h1>
+            <p className="text-zinc-400">
+              Please provide your name, email address, and phone number.
+            </p>
+          </div>
+
+          <div className="flex flex-col">
+            <label>Name</label>
+            <input type="text" placeholder="e.g. Stephen King" />
+          </div>
+
+          <div className="flex flex-col">
+            <label>Email Address</label>
+            <input type="text" placeholder="e.g. stephenking@lorem.com" />
+          </div>
+
+          <div className="flex flex-col">
+            <label>Phone Number</label>
+            <input type="number" placeholder="e.g. +1 234 567 890" />
+          </div>
+        </div>
+      </div>
+
+      <Navigation onSubmit={onSubmit} />
     </div>
   );
 };
