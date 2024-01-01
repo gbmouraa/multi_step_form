b4340e6c009cbb71d50660d9611ab99fdeb8fd7b
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-01 19:04:34 -0300

    feat: add step selected according to the current step

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,24 @@ import { useContext } from "react";
 import { FormContext } from "../formContext";
 
 const Header = () => {
-  const { steps } = useContext(FormContext);
+  const { steps, currentStep } = useContext(FormContext);
 
   return (
-    <header className="min-h-40 bg-mobile-aside">
+    <header className="min-h-40 bg-mobile-aside bg-cover bg-no-repeat">
       <ul className="flex justify-center gap-x-4 pt-10">
         {steps.map((item, idx) => {
           return (
             <li key={idx}>
-              <span className="flex h-9 w-9 items-center justify-center rounded-full border border-white font-medium text-white">
-                {idx + 1}
-              </span>
+              {currentStep === idx + 1 ? (
+                <span className="border-light-blue bg-light-blue text-marine-blue flex h-9 w-9 items-center justify-center rounded-full border font-bold">
+                  {idx + 1}
+                </span>
+              ) : (
+                <span className="flex h-9 w-9 items-center justify-center rounded-full border border-white font-bold text-white">
+                  {idx + 1}
+                </span>
+              )}
+
               <div className="hidden md:block">
                 <span>{item.step}</span>
                 <span>{item.desc}</span>
