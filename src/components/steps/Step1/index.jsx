2b7f8b7ab4240db97fad5e75bb0956fba4a4d832
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-01 19:05:30 -0300

    refactor: improving the structure of components

--- src/components/steps/Step1/index.jsx
+++
@@ -1,5 +0,0 @@
-const Step1 = () => {
-  return <h1>Step 1</h1>;
-};
-
-export default Step1;
---
+++ src/components/steps/StepOne.jsx
@@ -0,0 +1,9 @@
+const StepOne = () => {
+  return (
+    <div className="rounded-md bg-white">
+      <h1>Step 1</h1>
+    </div>
+  );
+};
+
+export default StepOne;
