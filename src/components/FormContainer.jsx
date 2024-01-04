280dc3d3509135a71f2088c3b39d0f53656a4e18
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-03 22:43:05 -0300

    feat: add function to render form component based on current step

--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -1,13 +1,35 @@
+import { useContext } from "react";
+import { FormContext } from "../formContext";
 import StepOne from "./steps/StepOne";
+import StepTwo from "./steps/StepTwo";
+import StepThree from "./steps/StepThree";
+import StepFour from "./steps/StepFour";
 
 const FormContainer = ({ children }) => {
+  const { currentStep } = useContext(FormContext);
+
+  const renderStep = () => {
+    switch (currentStep) {
+      case 1:
+        return <StepOne />;
+      case 2:
+        return <StepTwo />;
+      case 3:
+        return <StepThree />;
+      case 4:
+        return <StepFour />;
+      default:
+        return <StepOne />;
+    }
+  };
+
   return (
     <div className="relative h-screen w-full bg-magnolia md:flex md:h-fit md:w-fit md:rounded-xl md:bg-white md:p-4 md:shadow-md">
       {/* Header */}
       {children}
 
       {/* Form steps */}
-      <StepOne />
+      {renderStep()}
     </div>
   );
 };
