f81c79289fd4d6ba9079a55a0d757f373345c11f
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-03 21:17:20 -0300

    feat: Header ready

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   const { steps, currentStep } = useContext(FormContext);
 
   return (
-    <header className="md:bg-desktop-aside min-h-44  bg-mobile-aside bg-cover bg-center bg-no-repeat md:min-w-[260px] md:rounded-lg md:px-5">
+    <header className="min-h-44 bg-mobile-aside bg-cover bg-center bg-no-repeat md:h-[540px] md:min-w-[260px] md:rounded-lg md:bg-desktop-aside md:px-5">
       <ul className="flex justify-center gap-x-4 pt-10 md:flex-col md:gap-y-4">
         {steps.map((item, idx) => {
           return (
@@ -15,14 +15,14 @@ const Header = () => {
                   {idx + 1}
                 </span>
               ) : (
-                <span className="flex h-9 w-9 items-center justify-center rounded-[50%] border border-white font-bold text-white">
+                <span className="flex h-9 w-9 items-center justify-center rounded-[50%] border border-white font-bold text-magnolia">
                   {idx + 1}
                 </span>
               )}
 
               <div className="ml-4 hidden flex-col md:flex">
-                <span>{item.step}</span>
-                <span>{item.desc}</span>
+                <span className="text-sm text-pastel-blue">{item.step}</span>
+                <span className="font-medium text-magnolia">{item.desc}</span>
               </div>
             </li>
           );
