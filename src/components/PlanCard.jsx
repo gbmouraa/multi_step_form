4a9ab0a943030e3ceda7cdf31a3b4d511c2713f7
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-06 20:42:36 -0300

    feat: add StepTwo, componentized PlanCard

---
+++ src/components/PlanCard.jsx
@@ -0,0 +1,29 @@
+const PlanCard = (props) => {
+  return (
+    <div className="md:w-[31%]">
+      <input
+        className="peer absolute appearance-none"
+        type="radio"
+        name="plan"
+        id={props.id}
+      />
+      <label
+        htmlFor={props.id}
+        className="peer-label flex cursor-pointer items-center gap-x-3 rounded-lg border border-zinc-200 p-4 hover:bg-alabaster md:flex-col md:items-start"
+      >
+        <img src={props.icon} alt="illustration" className="md:mb-10" />
+        <div className="flex flex-col">
+          <span className="font-bold text-marine-blue">{props.title}</span>
+          <span className="text-sm text-zinc-400">
+            {props.yearly ? props.yr : props.mo}
+          </span>
+          {props.yearly && (
+            <span className="text-sm text-marine-blue">2 months free</span>
+          )}
+        </div>
+      </label>
+    </div>
+  );
+};
+
+export default PlanCard;
--- src/components/steps/StepTwo.jsx
+++ src/components/steps/StepTwo.jsx
@@ -1,9 +1,17 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Navigation from "../Navigation";
+import FormContent from "../FormContent";
 import { FormContext } from "../../formContext";
+import PlanCard from "../PlanCard";
+
+import arcadeIcon from "../../assets/icon-arcade.svg";
+import advancedIcon from "../../assets/icon-advanced.svg";
+import proIcon from "../../assets/icon-pro.svg";
 
 const StepTwo = () => {
   const { setCurrentStep } = useContext(FormContext);
+
+  const [yearly, setYearly] = useState(false);
 
   const onSubmit = () => {
     setCurrentStep((prevStep) => prevStep + 1);
@@ -14,10 +22,46 @@ const StepTwo = () => {
   };
 
   return (
-    <div>
-      <h1>Step Two</h1>
-      <Navigation onSubmit={onSubmit} goBack={goBack} />
-    </div>
+    <FormContent
+      title="Select your plan"
+      description="You have the option of monthly or yearly biling."
+      content={
+        <>
+          <div>
+            {/* inputs */}
+            <div className="flex flex-col gap-y-3 md:flex-row md:justify-between">
+              <PlanCard
+                id="arcade"
+                icon={arcadeIcon}
+                title="Arcade"
+                mo="$9/mo"
+                yr="$90/yr"
+                yearly={yearly}
+              />
+
+              <PlanCard
+                id="advanced"
+                icon={advancedIcon}
+                title="Advanced"
+                mo="$12/mo"
+                yr="$120/yr"
+                yearly={yearly}
+              />
+
+              <PlanCard
+                id="pro"
+                icon={proIcon}
+                title="Pro"
+                mo="$15/mo"
+                yr="$150/yr"
+                yearly={yearly}
+              />
+            </div>
+          </div>
+        </>
+      }
+      nav={<Navigation onSubmit={onSubmit} goBack={goBack} />}
+    />
   );
 };
 
