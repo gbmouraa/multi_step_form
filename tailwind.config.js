6dce2470b8e88701edfdc1a4557f6fc21ac19f3f
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-06 20:22:22 -0300

    style: customizing screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      md: "919px",
+    },
+
     fontFamily: {
       ubuntu: ["Ubuntu", "sans-serif"],
     },
