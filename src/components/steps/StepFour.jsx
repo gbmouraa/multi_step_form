351a1fa70e15eefc7e7583c15bfd66c7f4d260e8
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-10 19:17:49 -0300

    feat: add StepFour

--- src/components/steps/StepFour.jsx
+++ src/components/steps/StepFour.jsx
@@ -1,23 +1,109 @@
 import { useContext } from "react";
 import { FormContext } from "../../formContext";
+import FormContent from "../FormContent";
 import Navigation from "../Navigation";
 
 const StepFour = () => {
-  const { setCurrentStep } = useContext(FormContext);
+  const {
+    setCurrentStep,
+    plan,
+    isYearly,
+    onlineService,
+    largerStorage,
+    customizableProfile,
+  } = useContext(FormContext);
 
-  const onSubmit = () => {
-    setCurrentStep((prevStep) => prevStep + 1);
+  let totalPrice;
+  let price;
+  const planPrice = () => {
+    if (plan === "arcade") price = 9;
+    if (plan === "advanced") price = 12;
+    if (plan === "pro") price = 15;
+
+    if (isYearly) price = price * 10;
+    totalPrice = price;
   };
+
+  planPrice();
+
+  let addOns = [];
+  const calcAddOns = () => {
+    if (onlineService) {
+      addOns.push({ desc: "Online service", price: isYearly ? 10 : 1 });
+    }
+    if (largerStorage) {
+      addOns.push({ desc: "Larger storage", price: isYearly ? 20 : 2 });
+    }
+    if (customizableProfile) {
+      addOns.push({ desc: "Customizable Profile", price: isYearly ? 20 : 2 });
+    }
+  };
+
+  calcAddOns();
+
+  const calcTotalPrice = () => {
+    if (!addOns) return;
+
+    let addOnsPrice;
+    addOnsPrice = addOns.reduce((count, element) => count + element.price, 0);
+    totalPrice = totalPrice + addOnsPrice;
+  };
+
+  calcTotalPrice();
 
   const goBack = () => {
     setCurrentStep((prevStep) => prevStep - 1);
   };
 
   return (
-    <div>
-      <h1>Step Four</h1>
-      <Navigation onSubmit={onSubmit} goBack={goBack} />
-    </div>
+    <FormContent
+      title="Finishing up"
+      description="Double-check everything looks OK before confirming."
+      content={
+        <>
+          <div className="rounded-lg bg-alabaster p-4">
+            <div className="flex items-center justify-between border-b border-zinc-200 pb-3">
+              <div className="flex flex-col text-left">
+                <span className="font-bold capitalize text-marine-blue">
+                  {`${plan} (${isYearly ? "Yearly" : "Monthly"})`}
+                </span>
+                <button
+                  onClick={() => setCurrentStep(2)}
+                  className="w-fit text-sm text-zinc-400 underline"
+                >
+                  Change
+                </button>
+              </div>
+              <span className="font-bold text-marine-blue">{`$${price}/${
+                isYearly ? "yr" : "mo"
+              }`}</span>
+            </div>
+
+            {addOns &&
+              addOns.map((item, idx) => {
+                return (
+                  <div className="mt-3 flex justify-between" key={idx}>
+                    <span className="text-zinc-400">{item.desc}</span>
+                    <span className="font-medium text-marine-blue">{`+$${
+                      item.price
+                    }/${isYearly ? "yr" : "mo"}`}</span>
+                  </div>
+                );
+              })}
+          </div>
+          {/* total price */}
+          <div className="mt-5 flex justify-between px-3">
+            <span className="text-zinc-400">
+              Total (per {isYearly ? "yearly" : "month"})
+            </span>
+            <span className="font-bold text-purple-blue">{`+$${totalPrice}/${
+              isYearly ? "yr" : "mo"
+            }`}</span>
+          </div>
+        </>
+      }
+      nav={<Navigation goBack={goBack} confirm={() => setCurrentStep(5)} />}
+    />
   );
 };
 
